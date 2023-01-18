25a0bc66fce2ed0c3129c7fa548c4e5d665d3483
Ceciliamelo97 <melocecilia97@gmail.com>
2023-01-18 20:18:12 -0300

    fazer mudanças que Yuri pediu

--- Cecilia/ex006.py
+++ Cecilia/ex006.py
@@ -1,5 +1,5 @@
 n = int(input("Digite um número: "))
 d = n * 2
 t = n * 3
-r = n ** 2
+r = n ** (1/2)
 print("O dobro de {} é: {}\nO triplo é: {}\nA raiz quadrada é: {}" .format(n, d, t, r))
--- Cecilia/ex014.py
+++ Cecilia/ex014.py
@@ -1,3 +1,3 @@
 c = float(input("Digite a temperatura em ºC: "))
-f = ((9 * c) / 5) + 32
+f = c * 1.8 + 32
 print("A temperatura {}ºC equivale a {}ºF" .format(c, f))
